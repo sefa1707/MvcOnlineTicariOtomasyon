b9b93d0cd7a868f165e64cacd09d49ec63c0a31b
Sefa AŞIK <sefa.a1707@gmail.com>
2023-06-11 01:23:14 +0300

    Bölüm 5 sonu. -Proje teması eklendi. -AdminLayout View düzenlemesi yapıldı. -Kategori bölümü sayfaları oluşturuldu velisteleme, ekleme, silme, düzenleme işlemleri gerçekleştirildi.

---
+++ MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        Context db = new Context();
+        public ActionResult Index()
+        {
+            var kategori = db.Kategoris.ToList();
+            return View(kategori);
+        }
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategori k)
+        {
+            db.Kategoris.Add(k);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var ktg = db.Kategoris.Find(id);
+            db.Kategoris.Remove(ktg);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktg = db.Kategoris.Find(id);
+            return View("KategoriGetir", ktg);
+        }
+        public ActionResult KategoriGuncelle(Kategori k)
+        {
+            var ktg = db.Kategoris.Find(k.KategoriID);
+            ktg.KategoriAd = k.KategoriAd;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+     
+
+    }
+}
