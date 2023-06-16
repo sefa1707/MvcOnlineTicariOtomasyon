87bc2f7520622ef1f747e0996c19f256ec7e71f1
Sefa AŞIK <sefa.a1707@gmail.com>
2023-06-17 02:37:27 +0300

    Bölüm 8 sonu. -Cari Listeleme, -Cari Ekleme ve Düzenleme, *Cari pasife alma ve index sayfasında aktif cari listelenmesi yapılmalı *Aktif pasif carilerin ayrı listelendiği sayfaların ve carilerin aktif etme özelliğinin eklenmesi gerekiyor *Departman ve Kategori için validation controlleri oluştur.

---
+++ MvcOnlineTicariOtomasyon/Controllers/CariController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariController : Controller
+    {
+        // GET: Cari
+        Context db = new Context();
+        public ActionResult Index()
+        {
+            var cari = db.Caris.Where(x => x.CariDurum == true).ToList();
+            return View(cari);
+        }
+        [HttpGet]
+        public ActionResult CariEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CariEkle(Cari c)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("CariGetir");
+            }
+            db.Caris.Add(c);
+            c.CariDurum = true;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult CariPasifYap(int id)
+        {
+            var cari = db.Caris.Find(id);
+            cari.CariDurum = false;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CariGetir(int id)
+        {
+            var cari = db.Caris.Find(id);
+            return View("CariGetir", cari);
+        }
+        public ActionResult CariGuncelle(Cari c)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("CariGetir");
+            }
+            var cari = db.Caris.Find(c.CariID);
+            cari.CariAd = c.CariAd;
+            cari.CariSoyad = c.CariSoyad;
+            cari.CariSehir = c.CariSehir;
+            cari.CariMail = c.CariMail;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult CariSatis(int id)
+        {
+            var satis = db.SatisHarekets.Where(x => x.Cariid == id).ToList();
+            var caribul = db.Caris.Where(x => x.CariID == id).Select(y => y.CariAd + " " + y.CariSoyad).FirstOrDefault();
+            ViewBag.cariad = caribul;
+            return View(satis);
+        }
+    }
+}
--- MvcOnlineTicariOtomasyon/Models/Siniflar/Cari.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Cari.cs
@@ -12,17 +12,22 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         [Key]
         public int CariID { get; set; }
         [Column(TypeName = "Varchar")]
-        [StringLength(30)]
+        [StringLength(30, ErrorMessage ="En fazla 30 karakter yazabilirsiniz.")]
+        [Required(ErrorMessage ="Bu alanı boş geçemezsiniz")]
         public string CariAd { get; set; }
         [Column(TypeName = "Varchar")]
-        [StringLength(30)]
+        [StringLength(30, ErrorMessage = "En fazla 30 karakter yazabilirsiniz.")]
+        [Required(ErrorMessage = "Bu alanı boş geçemezsiniz")]
         public string CariSoyad { get; set; }
         [Column(TypeName = "Varchar")]
-        [StringLength(15)]
+        [StringLength(15, ErrorMessage = "En fazla 15 karakter yazabilirsiniz.")]
+        [Required(ErrorMessage = "Bu alanı boş geçemezsiniz")]
         public string CariSehir { get; set; }
         [Column(TypeName = "Varchar")]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "En fazla 50 karakter yazabilirsiniz.")]
+        [Required(ErrorMessage = "Bu alanı boş geçemezsiniz")]
         public string CariMail { get; set; }
+        public bool CariDurum { get; set; }
         public ICollection<SatisHareket> SatisHarekets { get; set; }
     }
 }
