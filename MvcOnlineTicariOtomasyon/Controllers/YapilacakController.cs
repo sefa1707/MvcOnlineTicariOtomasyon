f4d43df47e616bc20e672f1a99f9b7f6de105d2b
Sefa AŞIK <sefa.a1707@gmail.com>
2023-06-29 15:33:49 +0300

    Bölüm 17 sonu.

---
+++ MvcOnlineTicariOtomasyon/Controllers/YapilacakController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class YapilacakController : Controller
+    {
+        // GET: Yapilacak
+        Context db = new Context();
+        public ActionResult Index()
+        {
+            var carisay = db.Caris.Count().ToString();
+            ViewBag.carisayi = carisay;
+            var urunsay = db.Uruns.Count().ToString();
+            ViewBag.urunsayi = urunsay;
+            var satissay = db.SatisHarekets.Count().ToString();
+            ViewBag.satissayi = satissay;
+            var kategorisay = db.Kategoris.Count().ToString();
+            ViewBag.kategorisayi = kategorisay;
+            var yapilacak = db.Yapilacaks.ToList();
+            return View(yapilacak);
+        }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Yapilacak.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Yapilacak
+    {
+        [Key]
+        public int YapilacakID { get; set; }
+        [Column(TypeName="Varchar")]
+        [StringLength(100)]
+        public string YapilacakBaslik { get; set; }
+        [Column(TypeName = "bit")]
+        public bool YapilacakDurum { get; set; }
+
+    }
+}
